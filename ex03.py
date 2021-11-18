dd539d305bf4dc42878fe526080a154d5d8ec396
Samuel Santos <samuel.cezar@ykt.com.br>
2021-11-17 22:21:07 -0300

    Ex03 pronto

---
+++ ex03.py
@@ -0,0 +1,19 @@
+notas = []
+item = ''
+i = 0
+while i<5:
+    n = int(input('Insira a nota do aluno ' + str(i) + ': '))
+    notas.append(n);
+    i = i + 1
+
+i=0
+max1 = 0
+
+while i<5:
+    if (notas[i] > max1):
+        max1 = notas[i]
+        pos = i
+    i = i + 1
+
+print('\nA maior nota foi', max1);
+print('Essa nota foi obtida pelo aluno ', pos);
