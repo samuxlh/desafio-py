a232e58f57d77428f917074778f58b3ab59dcf51
Samuel Santos <samuel.cezar@ykt.com.br>
2021-11-19 09:40:47 -0300

    + ex01 is now working

--- ex01.py
+++ ex01.py
@@ -1,6 +1,8 @@
 lista = list(range(5000000))
 count = 0
 for x in lista:
-  if (lista[x] % 2 == 0 & lista[x] % 49 == 0 & lista[x] % 37):
-      count += 1
-print(count)
+  if (lista[x] % 2 == 0):
+    if(lista[x] % 49 == 0):
+          if(lista[x] % 37 == 0):
+                count = count + 1
+print('Existem', count, 'números que satisfazem a condição.')
